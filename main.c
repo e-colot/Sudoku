28cfdff56f1b15e51a1ed1acaa48c45fe374c2ce
E-was-here <168428088+E-was-here@users.noreply.github.com>
2024-08-15 17:22:51 +0200

    updated to follow specialized files

--- main.c
+++ main.c
@@ -1,27 +1,20 @@
-#include <SDL.h>
 #include <stdio.h>
+#include <stdlib.h>  // for dynamic memory allocation
+
+#include "grid.h"
+#include "graphics.h"
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("Error initializing SDL: %s\n", SDL_GetError());
-        return 1;
+    
+    char **grid = initGrid();
+    struct graphicsController graphics = initGraphics();
+    
+    if (grid != NULL && graphics.window != NULL && graphics.renderer != NULL) {
+        printf("Check-up complete");
     }
 
-    SDL_Window* window = SDL_CreateWindow("Hello, SDL2",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640, 480, 0);
-
-    if (!window) {
-        printf("Error creating window: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Delay(3000); // Wait 3 seconds
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    destroyGraphics(graphics);
+    freeGrid(grid);
     return 0;
 }
+
